7acd0231c7937489d39dc3f512b1476d6d2ae2d9
Scott Forbes <forbesst196@gmail.com>
2014-01-29 22:58:30 -0500

    Menu Start
    Made the menu

---
+++ Program001.py
@@ -0,0 +1,9 @@
+menu = """	
+			(A)dd a criter
+			(D)elete a critter
+			(L)ist all critters
+			(R)eload critter database
+			(S)ave critter database
+			(V)iew a critter
+			e(X)it""" # end of menu
+print(menu) # prints menu
